2671979bef298693dc11a158a1513bc1c59ad0b6
josephmolina256 <128499367+josephmolina256@users.noreply.github.com>
2023-11-12 15:38:29 -0500

    scaling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '2240px',
+        // => @media (min-width: 2240px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    }
+  }
